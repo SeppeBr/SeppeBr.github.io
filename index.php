43624fe5b0084fd3fb1a087e4613ba68ca0ab12f
SeppeBr <seppebremmers@gmail.com>
2021-09-13 11:09:55 +0200

    betekenis volle message

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+
+    <div class="w3-sidebar w3-bar-block w3-red" style="width:5%">
+        <a href="#" class="w3-bar-item w3-button">Link 1</a>
+        <a href="#" class="w3-bar-item w3-button">Link 2</a>
+        <a href="#" class="w3-bar-item w3-button">Link 3</a>
+    </div>
+
+    <div style="margin-left:25%">
+    </div>
+    <ul>
+        <li><a class="active" href="index.html">Home</a></li>
+        <li><a href="profile.html">Profiel</a></li>
+        <li><a href="blog.html">Blog</a></li>
+        <li style="float:right"><a href="#about">About</a></li>
+    </ul>
+
+</body>
+
+</html>
